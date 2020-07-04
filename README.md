865436c2a3e4ec12a43c2aa0ef9baf0259659da8
Manuel Schneider <manuel.schneider@dornbirn.at>
2020-07-04 15:46:01 +0200

    result of my-team-page

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# devchallanges-my-team-page
+# devchallenges.io | My Team page
+
+This is the result of the [challenge - My Team Page](https://devchallenges.io/challenges/hhmesazsqgKXrTkYkt0U).
+
+## Stack
+
+-   VS Code
+-   Tailwind CSS
+-   PurgeCSS
+-   PostCSS
+
+## Demo
+
+You can view the result on [dev.schman.at/my-team-page](http://dev.schman.at/my-team-page)
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    purge: [],
+    theme: {
+        extend: {
+            fontFamily: {
+                heading: ['"PT Serif"', 'sans-serif'],
+                text: ['"Poppins"', 'sans-serif'],
+                footer: ['"Montserrat"', 'sans-serif'],
+            },
+            colors: {
+                background: '#FAFAFA',
+                footer: '#A9A9A9',
+            },
+            spacing: {
+                xs: '0.625rem',
+                xxl: '5.3125rem',
+            },
+            fontSize: {
+                xxs: '0.625rem',
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+};
